ad5b75183a2a5a7e367ace3fa10aaac3c17330ae
Harry Garrood <hdgarrood@gmail.com>
2012-06-21 14:07:24 +0100

    remove --data-dir
    not needed since i can switch between self contained and standalone
    formats

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@ TYPE="--standalone"
 if [[ "$1" = "-c" ]]; then
     TYPE="--self-contained"
 fi
-pandoc -t slidy $TYPE --data-dir=data -c style.css slides.txt > presentation.html
+pandoc -t slidy $TYPE -c css/style.css slides.txt > presentation.html
